4389b0e74e1d2f66f4cf1ab5ea2ac261441536eb
areotwister <areotwister@gmail.com>
2024-05-01 12:58:12 +0200

    before change to kmonad layout

--- modules/nixos/kmonad.nix
+++ modules/nixos/kmonad.nix
@@ -16,6 +16,7 @@
   allow-cmd true
 )
 
+;; https://en.wikipedia.org/wiki/British_and_American_keyboards#/media/File:KB_United_States-NoAltGr.svg
 (defsrc
   esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12        ssrq slck pause
   grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc  ins  home pgup  nlck kp/  kp*  kp-
@@ -46,13 +47,19 @@
   mbc  M-bspc
   sbc  M-S-bspc
   srt  M-S-ret
-  st (around lshift tab)
-  ü #(ralt " u)
-  ö #(ralt " o)
-  ä #(ralt " a)
-  Ü #(ralt " U)
-  Ö #(ralt " O)
-  Ä #(ralt " A)
+  mgv  M-grv
+  sgv  M-S-grv
+  mbl  M-\\
+  sbl  M-S-\\
+  st   (around lshift tab)
+  clt  C-left
+  crt  C-right
+  ü    #(ralt " u)
+  ö    #(ralt " o)
+  ä    #(ralt " a)
+  Ü    #(ralt " U)
+  Ö    #(ralt " O)
+  Ä    #(ralt " A)
 )
 
 (deflayer base
@@ -67,7 +74,7 @@
 (deflayer shifted
   esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12        ssrq slck pause
   °    !    "    §    $    %    &    /    \(   \)   =    ?    `    bspc  ins  home pgup  nlck kp/  kp*  kp-
-  @st  Q    W    E    R    T    Z    U    I    O    P    |     *    ret   del  end  pgdn  kp7  kp8  kp9  kp+
+  @st  Q    W    E    R    T    Z    U    I    O    P    |    *    ret   del  end  pgdn  kp7  kp8  kp9  kp+
   caps A    S    D    F    G    H    J    K    L    ]    }    '                          kp4  kp5  kp6
   lsft >    Y    X    C    V    B    N    M    ;    :    \_   rsft            up         kp1  kp2  kp3  kprt
   lctl lmet lalt          S-spc                 ralt rmet cmp  rctl       left down rght  kp0  kp.
@@ -75,18 +82,18 @@
 
 (deflayer super
   esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12        ssrq slck pause
-  @mbc M-1  M-2  M-3  M-4  M-5  M-6  M-7  M-8  M-9  M-0  M-ß  M-´  @mbc  ins  home pgup  nlck kp/  kp*  kp-
+  @mgv M-1  M-2  M-3  M-4  M-5  M-6  M-7  M-8  M-9  M-0  M-ß  M-´  @mbc  ins  home pgup  nlck kp/  kp*  kp-
   tab  M-q  M-w  M-e  M-r  M-t  M-z  M-u  M-i  M-o  M-p  M-\  M-+  M-ret del  end  pgdn  kp7  kp8  kp9  kp+
-  caps M-a  M-s  M-d  M-f  M-g  M-h  M-j  M-k  M-l  M-[  M-{  M-#                        kp4  kp5  kp6
+  caps M-a  M-s  M-d  M-f  M-g  M-h  M-j  M-k  M-l  M-[  M-{  @mbl                       kp4  kp5  kp6
   @spr M-<  M-y  M-x  M-c  M-v  M-b  M-n  M-m  M-,  M-.  M--  rsft            up         kp1  kp2  kp3  kprt
   lctl lmet lalt           spc                 @agr rmet cmp  rctl       left down rght  kp0  kp.
 )
 
 (deflayer supershifted
   esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12        ssrq slck pause
-  M-°  @sp1 @sp2 @sp3 @sp4 @sp5 @sp6 @sp7 @sp8 @sp9 @sp0 ?    `    @sbc  ins  home pgup  nlck kp/  kp*  kp-
+  @sgv @sp1 @sp2 @sp3 @sp4 @sp5 @sp6 @sp7 @sp8 @sp9 @sp0 ?    `    @sbc  ins  home pgup  nlck kp/  kp*  kp-
   tab  M-Q  M-W  M-E  M-R  M-T  M-Z  M-U  M-I  M-O  M-P  M-|  M-*  @srt  del  end  pgdn  kp7  kp8  kp9  kp+
-  caps M-A  M-S  M-D  M-F  M-G  M-H  M-J  M-K  M-L  M-]  M-}  M-'                        kp4  kp5  kp6
+  caps M-A  M-S  M-D  M-F  M-G  M-H  M-J  M-K  M-L  M-]  M-}  @sbl                       kp4  kp5  kp6
   lsft M->  M-Y  M-X  M-C  M-V  M-B  M-N  M-M  M-;  M-:  M-\_ rsft            up         kp1  kp2  kp3  kprt
   lctl lmet lalt           spc                 ralt rmet cmp  rctl       left down rght  kp0  kp.
 )
@@ -112,9 +119,9 @@
 (deflayer capsvim
   _    _    _    _    _    _    _    _    _    _    _    _    _          _    _    _
   _    _    _    _    end  _    _    _    _    _    home _    _    _     _    _    _    _    _    _    _
-  _    _    _    _    _    _    _    _    _    _    _    _    _    _     _    _    _    _    _    _    _
+  _    _    @crt _    _    _    _    _    _    _    _    _    _    _     _    _    _    _    _    _    _
   caps _    _    _    _    _    left down up   rght _    _    _                         _    _    _
-  _    _    _    _    _    _    _    _    _    _    _    _    _               _         _    _    _    _
+  _    _    _    _    _    _    @clt _    _    _    _    _    _               _         _    _    _    _
   _    _    _              _                   _    _    _    _          _    _    _    _    _
 )
          '';
